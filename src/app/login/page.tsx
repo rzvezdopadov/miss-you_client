9e0a714854b636f5b315bc3512dbf91c193103ab
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2024-02-24 19:51:24 +0300

    change base pages for tests

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <div>
-            <h1 style={{ backgroundColor: `#000` }}>О нас</h1>
+            <h1 style={{ backgroundColor: `#000` }}>Войти</h1>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link';
-
 export default function Page() {
     return (
         <div>
-            <h1>Home</h1>
-            <Link href="/about">About</Link>
+            <h1 style={{ backgroundColor: `#000` }}>Главная</h1>
         </div>
     );
 }
