5e094819e31567585bb8de28a895a9ef52ebc8ec
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2024-03-01 16:32:57 +0300

    added function

--- src/entities/Token/index.ts
+++ src/entities/Token/index.ts
@@ -1,5 +1,6 @@
+import { getToken } from './model/selectors/modelSelectors';
 import { tokenReducer, tokenActions } from './model/slices/tokenSlice';
 import { TokenSchema } from './model/types/token.types';
 
-export { tokenReducer, tokenActions };
+export { tokenReducer, tokenActions, getToken };
 export type { TokenSchema };
