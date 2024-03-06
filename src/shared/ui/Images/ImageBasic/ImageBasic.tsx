9dbc9cf847d7793192e02c6ec4bbb915a08e6986
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2024-03-06 14:58:41 +0300

    fix problem with classes

--- src/shared/ui/Images/ImageBasic/ImageBasic.tsx
+++ src/shared/ui/Images/ImageBasic/ImageBasic.tsx
@@ -3,7 +3,7 @@ import { Image } from 'antd';
 import { ImageProps } from 'antd/es/image';
 import cls from './ImageBasic.module.scss';
 
-type rounded = 'not' | 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl' | 'full';
+type rounded = 'not' | 's' | 'm' | 'l' | 'xl' | 'xl2' | 'xl3' | 'xl4' | 'xl5' | 'xl6' | 'full';
 
 interface ImageBasicProps extends ImageProps {
     rounded?: rounded;
